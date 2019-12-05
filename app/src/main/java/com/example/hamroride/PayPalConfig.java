7fcb72070d66ebc8af7ce33e4dabdb394b19f1fa
sandis <sandisprajapati52@gmail.com>
2019-12-04 17:38:43 -0800

    paypal key added

---
+++ app/src/main/java/com/example/hamroride/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.example.hamroride;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AXGlZJ-6rLnwBMhHMkuaQSpdbKyBIbcwWiIUJvBeCO7qEvdUI2DiM_b2biBZzydhXski8KUkpF55zCOl";
+}
+
---
+++ app/src/main/java/com/example/hamroride/onAppKilled.java
@@ -0,0 +1,20 @@
+package com.example.hamroride;
+
+public class onAppKilled extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("driversAvailable");
+        GeoFire geoFire = new GeoFire(ref);
+        geoFire.removeLocation(userId);
+    }
+}
+
